0c201e7bf51a3e801b7ac18aef44beb6a9539359
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2024-11-26 18:33:40 +0000

    Add initial token code with tests
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

---
+++ spec/bolt/puppetfile/installer_spec.rb
@@ -0,0 +1,28 @@
+# spec/bolt/puppetfile/installer_spec.rb
+require 'bolt/puppetfile/installer'
+
+describe Bolt::Puppetfile::Installer do
+  context 'when BOLT_FORGE_TOKEN is not set' do
+    before { ENV['BOLT_FORGE_TOKEN'] = nil }
+
+    it 'raises an error' do
+      expect { Bolt::Puppetfile::Installer.new }.to raise_error(Bolt::Error, /BOLT_FORGE_TOKEN is not set/)
+    end
+  end
+
+  context 'when BOLT_FORGE_TOKEN is invalid' do
+    before { ENV['BOLT_FORGE_TOKEN'] = 'invalid token' }
+
+    it 'raises an error' do
+      expect { Bolt::Puppetfile::Installer.new }.to raise_error(Bolt::Error, /BOLT_FORGE_TOKEN is invalid/)
+    end
+  end
+
+  context 'when BOLT_FORGE_TOKEN is valid' do
+    before { ENV['BOLT_FORGE_TOKEN'] = 'validtoken123' }
+
+    it 'does not raise an error' do
+      expect { Bolt::Puppetfile::Installer.new }.not_to raise_error
+    end
+  end
+end
